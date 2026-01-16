73144a90b48acbde54b55cb3f2e673f45061c23e
Manikantha M <manikantha.m@neutrinos.co>
2026-01-16 15:50:15 +0300

    re order files

---
+++ src/tutorial.ts
@@ -0,0 +1,10 @@
+console.log('typescript tutorial');
+interface someValue {
+    name: string,
+    id: number
+};
+let someObj:someValue ={
+    name:'random',
+    id:123
+};
+console.log(someObj)
